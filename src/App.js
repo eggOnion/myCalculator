04a9a45fa5044693ec7ef2eabc40383b66c59948
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2024-10-06 18:34:11 +0800

    added ReadMe URL link to github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <Calculator />
+      <p />
+      <a
+          className="App-link"
+          href="https://github.com/eggOnion/myCalculator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more about the App & my learning experiences 
+      </a>
     </div>
   );
 }
